0caa53541ea6e2cb9415faceb68eb7ec9ca94b60
JPhusion <jfusion3600@gmail.com>
2022-04-11 17:27:51 +1000

    Linux Version

--- usb detection.py
+++ usb detection.py
@@ -1,10 +1,8 @@
 import wmi
 
-raw_wql = "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA \'Win32_USBHub\'"
 device_connected_wql = "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA \'Win32_USBHub\'"
 device_disconnected_wql = "SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA \'Win32_USBHub\'"
 c = wmi.WMI()
-# watcher = c.watch_for(raw_wql=raw_wql)
 connected_watcher = c.watch_for(raw_wql=device_connected_wql)
 disconnected_watcher = c.watch_for(raw_wql=device_disconnected_wql)
 
