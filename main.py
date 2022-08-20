317d84217ad525e63e55e6f19329305c45065d70
RPI JOSH CHAN <22j.chan@sydgram.nsw.edu.au>
2022-08-20 10:17:26 +1000

    Fullscreen bug fix

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ import sys
 
 # os.environ['SDL_VIDEODRIVER'] = "dummy"
 # os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-os.environ['SDL_VIDEO_WINDOW_POS'] = f"{0},{0}"
+os.environ['SDL_VIDEO_WINDOW_POS'] = f"{-5},{-30}"
 os.environ['PYGAME_FREETYPE'] = '1'
 
 import pygame
+import pygame.freetype
 
 # init window
 clock = pygame.time.Clock()
