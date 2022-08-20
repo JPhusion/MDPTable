2f433936eb9b4106ba653d2f1aabd6c545feb416
JPhusion <jfusion3600@gmail.com>
2022-08-20 11:47:46 +1000

    Auto update on startup

--- commands/update.py
+++ commands/update.py
@@ -5,6 +5,6 @@ import sys
 def update():
     # test
     repo = git.Repo('./')
-    if "0" not in repo.git.rev_list('--left-right', '--count', f'{"main"}...{"main"}@{{u}}'):
+    if "0" not in repo.git.rev_list('--left-right', '--count', f'{"main"}...{"main"}@{{u}}').split('\t')[1]:
         repo.remotes.origin.pull()
         os.execl(sys.executable, sys.executable, *sys.argv)
