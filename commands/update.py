7dd0176687ffd82910097db713ea22559c9f3718
JPhusion <jfusion3600@gmail.com>
2022-08-20 11:36:53 +1000

    Auto update on startup

--- commands/update.py
+++ commands/update.py
@@ -5,6 +5,8 @@ import sys
 def update():
     # TEST
     repo = git.Repo('./')
+    print(list(repo.iter_commits('main..main@{u}')))
+    print(list(repo.iter_commits('main@{u}..main')))
     if list(repo.iter_commits('main..main@{u}')):
         repo.remotes.origin.pull()
         os.execl(sys.executable, sys.executable, *sys.argv)
