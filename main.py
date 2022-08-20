199ce400ebfcccee50938a1c1bc6001419c59524
JPhusion <jfusion3600@gmail.com>
2022-08-20 11:06:47 +1000

    Adjust screen position

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import sys
 
 # os.environ['SDL_VIDEODRIVER'] = "dummy"
 # os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-os.environ['SDL_VIDEO_WINDOW_POS'] = f"{-100},{-30}"
+os.environ['SDL_VIDEO_WINDOW_POS'] = f"{-1200},{-30}"
 os.environ['PYGAME_FREETYPE'] = '1'
 
 import pygame
