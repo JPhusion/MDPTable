f6106f2e6cfdfd078da3ad39f094c82e12d5613a
JPhusion <jfusion3600@gmail.com>
2022-08-23 17:48:45 +1000

    Aug 23 In House Coding

--- commands/signin.py
+++ commands/signin.py
@@ -2,5 +2,5 @@ from gui.widgets.weather import *
 from gui.widgets.covid import *
 from gui.widgets.clock import *
 
-def signin(command, widgets):
+def signin():
     return [widget_clock(3, 2, (0,0)), widget_covid(3, 2, (3,0))]
