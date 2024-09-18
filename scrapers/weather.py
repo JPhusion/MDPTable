f8d2b82200347ff98998c251475561aa4ba8c945
Josh Chan <jfusion3600@gmail.com>
2024-09-18 19:55:58 +1000

    removed calendar

--- scrapers/weather.py
+++ scrapers/weather.py
@@ -10,7 +10,7 @@ def call():
     # API_key = "87a63db4499dfd7bc82e0bd0306f3240"
     API_key = "ce8c3c7d8000ca207b7e994530375e2e"
     response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={API_key}")
+        f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code&timeformat=unixtime&timezone=Australia%2FSydney&forecast_days=1")
     return response.json()
 
 def tempNow(data):
@@ -24,3 +24,7 @@ def descNow(data):
 
 def icon(data):
     return data["current"]["weather"][0]["icon"]
+
+if __name__ == "__main__":
+    data = call()
+    print(data)
