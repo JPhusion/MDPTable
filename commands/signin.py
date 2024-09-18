e8525ee16e8e8831ee6f47bcc6ae94614f98437f
Josh Chan <jfusion3600@gmail.com>
2024-09-18 19:53:12 +1000

    Changed startup widgets

--- commands/signin.py
+++ commands/signin.py
@@ -4,4 +4,5 @@ from gui.widgets.clock import *
 from gui.widgets.calendar import *
 
 def signin():
-    return [widget_clock(3, 2, (3,0)), widget_covid(3, 2, (3,2)), widget_weather(3, 2, (0, 0)), widget_cal(3, 3, (0, 2))]
+    # return [widget_clock(3, 2, (3,0)), widget_covid(3, 2, (3,2)), widget_weather(3, 2, (0, 0)), widget_cal(3, 3, (0, 2))]
+    return [widget_clock(3, 2, (3,0)), widget_covid(3, 2, (3,2)), widget_cal(3, 3, (0, 2))]
