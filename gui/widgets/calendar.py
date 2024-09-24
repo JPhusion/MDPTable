99948a2f3071e3c5a73eaabdfd6e79461b9f9b08
Josh Chan <jfusion3600@gmail.com>
2024-09-24 13:15:26 +1000

    typo

--- gui/widgets/calendar.py
+++ gui/widgets/calendar.py
@@ -95,6 +95,6 @@ class widget_cal(widget):
             ["Pubcrawl with Friends", "9pm", "The Argyle, The Rocks"],
             # ["MATH2069 Tutorial", "Tomorrow 12pm", "H13 Lawernce East 1041"],
             ["MATH2069 Lecture", "Tomorrow 2pm", "Keith Burrows Theatre"],
-            ["Bouldering with Eric", "Tomorrow 5pm", "ClimbFit Maquorie"],
+            ["Bouldering with Eric", "Tomorrow 5pm", "ClimbFit Macquarie"],
             ["rUNSWift Lab Session", "Saturday 10am", "Ainsworth Level 5"],
         ]
